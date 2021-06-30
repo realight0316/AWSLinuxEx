aeb6887bc2621e381b08cc116c5d1147977bf309
realight0316 <0316kjh@naver.com>
2021-06-30 15:22:47 +0900

    Add second file

---
+++ first.c
@@ -0,0 +1,7 @@
+// github.com/AWSLinuxEx repository와 연결 시도
+
+#include <stdio.h>
+
+void main(){
+    printf("Hello World!\n");
+}
---
+++ secondfile.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void main(void){
+    printf("Second file : 22222\n");
+
+}
